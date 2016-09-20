0f4df4f1c4d50c29b036848e9ab6495fb9a6ff0e
Junbin Deng <junbin.deng@gmail.com>
2016-09-20 09:46:03 +0800

    Update Algorithms, Part I - Assignment 2: Queues

---
+++ src/Deque.java
@@ -0,0 +1,67 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    /**
+     * Construct an empty deque.
+     */
+    public Deque() {
+
+    }
+
+    /**
+     * Is the deque empty?
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Return the number of items on the deque.
+     */
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Add the item to the front.
+     */
+    public void addFirst(Item item) {
+
+    }
+
+    /**
+     * Add the item to the end.
+     */
+    public void addLast(Item item) {
+
+    }
+
+    /**
+     * Remove and return the item from the front.
+     */
+    public Item removeFirst() {
+        return null;
+    }
+
+    /**
+     * Remove and return the item from the end.
+     */
+    public Item removeLast() {
+        return null;
+    }
+
+    /**
+     * Return an iterator over items in order from front to end.
+     */
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    /**
+     * Unit testing.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RandomizedQueue.java
@@ -0,0 +1,61 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    /**
+     * Construct an empty randomized queue.
+     */
+    public RandomizedQueue() {
+
+    }
+
+    /**
+     * Return an independent iterator over items in random order.
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    /**
+     * Is the queue empty?
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Return the number of items on the queue.
+     */
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Add the item.
+     */
+    public void enqueue(Item item) {
+
+    }
+
+    /**
+     * Remove and return a random item.
+     */
+    public Item dequeue() {
+        return null;
+    }
+
+    /**
+     * Return (but do not remove) a random item.
+     */
+    public Item sample() {
+        return null;
+    }
+
+    /**
+     * Unit testing
+     */
+    public static void main(String[] args) {
+
+    }
+}
