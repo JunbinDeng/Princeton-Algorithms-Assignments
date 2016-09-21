0e0951e72c2b4f7886e3b0b94e0d2c9b12da96df
Junbin Deng <junbin.deng@gmail.com>
2016-09-21 18:22:30 +0800

    Update Algorithms, Part I - Assignment 2: Queues

--- src/Deque.java
+++ src/Deque.java
@@ -1,67 +1,167 @@
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int size;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
 
     /**
      * Construct an empty deque.
      */
     public Deque() {
-
+        // Empty
     }
 
     /**
      * Is the deque empty?
      */
     public boolean isEmpty() {
-        return false;
+        return first == null;
     }
 
     /**
      * Return the number of items on the deque.
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
      * Add the item to the front.
      */
     public void addFirst(Item item) {
-
+        checkItem(item);
+        Node node = new Node();
+        node.item = item;
+        if (isEmpty()) {
+            node.next = null;
+            last = first = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+        size++;
     }
 
     /**
      * Add the item to the end.
      */
     public void addLast(Item item) {
-
+        checkItem(item);
+        Node node = new Node();
+        node.item = item;
+        node.next = null;
+        if (isEmpty()) {
+            last = first = node;
+        } else {
+            last = last.next = node;
+        }
+        size++;
     }
 
     /**
      * Remove and return the item from the front.
      */
     public Item removeFirst() {
-        return null;
+        checkRemovable();
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
+        }
+        size--;
+        return item;
     }
 
     /**
      * Remove and return the item from the end.
      */
     public Item removeLast() {
-        return null;
+        checkRemovable();
+        Item item = last.item;
+        for (Node current = first; current != null && current.next != null; current = current.next) {
+            if (current.next.next != null) {
+                continue;
+            }
+            current.next = null;
+            last = current;
+            size--;
+        }
+        return item;
     }
 
     /**
      * Return an iterator over items in order from front to end.
      */
+    @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new DequeIterator();
+    }
+
+    private void checkItem(Item item) {
+        if (item == null) {
+            throw new NullPointerException("Cannot add a null item to deque.");
+        }
+    }
+
+    private void checkRemovable() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot remove item from an empty deque.");
+        }
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (current == null) {
+                throw new NoSuchElementException("No more next item in deque.");
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Cannot remove item in deque.");
+        }
     }
 
     /**
      * Unit testing.
      */
     public static void main(String[] args) {
+        String item1 = "1";
+        String item2 = "2";
+        String item3 = "3";
+        String item4 = "4";
 
+        Deque<String> items = new Deque<>();
+        items.addFirst(item2);
+        items.addLast(item3);
+        items.addFirst(item1);
+        items.addLast(item4);
+        items.removeFirst();
+        items.removeLast();
+
+        for (String item : items) {
+            StdOut.print(item + " ");
+        }
     }
 }
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,41 +1,53 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Node first;
+    private int size;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
 
     /**
      * Construct an empty randomized queue.
      */
     public RandomizedQueue() {
-
-    }
-
-    /**
-     * Return an independent iterator over items in random order.
-     */
-    @Override
-    public Iterator<Item> iterator() {
-        return null;
+        // Empty
     }
 
     /**
      * Is the queue empty?
      */
     public boolean isEmpty() {
-        return false;
+        return first == null;
     }
 
     /**
      * Return the number of items on the queue.
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
      * Add the item.
      */
     public void enqueue(Item item) {
-
+        Node node = new Node();
+        node.item = item;
+        node.next = null;
+        if (isEmpty()) {
+            first = node;
+        } else {
+            Node oldFirst = first;
+            first = node;
+            first.next = oldFirst;
+        }
+        size++;
     }
 
     /**
@@ -49,6 +61,29 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * Return (but do not remove) a random item.
      */
     public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot query item from an empty deque.");
+        }
+        int index = StdRandom.uniform(size);
+        Node current = first;
+        for (int i = 0; i < size; i++) {
+            if (index == i) {
+                return current.item;
+            }
+            if (current.next != null) {
+                current = current.next;
+            } else {
+                break;
+            }
+        }
+        return current.item;
+    }
+
+    /**
+     * Return an independent iterator over items in random order.
+     */
+    @Override
+    public Iterator<Item> iterator() {
         return null;
     }
 
