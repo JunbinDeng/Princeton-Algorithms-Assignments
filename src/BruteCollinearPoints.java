cf92c368a4bdfe620d1c72945cf9e944ab34bedd
Junbin Deng <junbin.deng@gmail.com>
2016-09-26 18:29:29 +0800

    Update Algorithms, Part I - Assignment 3: Pattern Recognition

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,24 @@
+
+public class BruteCollinearPoints {
+
+    /**
+     * Finds all line segments containing 4 points.
+     */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments.
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * The line segments.
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,24 @@
+
+public class FastCollinearPoints {
+
+    /**
+     * Finds all line segments containing 4 or more points.
+     */
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments.
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * The line segments.
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
